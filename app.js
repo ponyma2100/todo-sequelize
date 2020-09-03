92cb67bcbed60015a876e2bf88a7698a9b64d777
Pony <ponyma2100@gmail.com>
2020-09-03 19:05:18 +0800

    feat: add login and register page

--- app.js
+++ app.js
@@ -16,6 +16,27 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/regin', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+
 app.listen(PORT, (req, res) => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
